b398c5bc5daeb2e6e299bf04a8b25f57b12c5cb3
th2empty <th2empty@gmail.com>
2022-03-28 22:05:22 +0500

    обновил setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,39 @@
+function downloadGolang() {
+  echo -e "\033[36m ⌛ detecting system..."
+  arch=$(uname -m)
+  if [ "$arch" == "x86_64" ]; then
+    echo -e "\033[32m OK ✓"
+    echo -e "\033[36m ⌛ downloading golang..."
+    cd /tmp
+    wget https://go.dev/dl/go1.18.linux-amd64.tar.gz
+    echo -e "\033[32m golang downloaded ✓"
+    echo -e "\033[36m ⌛ extracting golang..."
+    sudo tar -C /usr/local/ -xzf go1.18.linux-amd64.tar.gz
+    mkdir /usr/lib/go
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+    source ~/.bashrc
+    echo -e "\033[32m golang installed ✓"
+    echo -e "\033[33m reboot required "
+    return 0
+  else
+    echo -e "\033[31m [ERROR]: detecting system failed ✕\n install golang manually"
+  fi
+}
+
+function checkGolang() {
+    PKG=$(go version | grep go)
+    echo -e "\033[36m Checking..."
+    if [ -n "$PKG" ]; then
+        echo -e "\033[32m installed ✓"
+    else
+        downloadGolang
+    fi
+}
+
 python3.8 -V || apt install python3.8
-go version || apt install golang
+checkGolang
 
 echo "Package installing..."
 echo -n "HTTProuter " & go get github.com/julienschmidt/httprouter && echo "success!" || echo "FAIL!"
